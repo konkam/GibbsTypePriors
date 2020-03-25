ff10c5d4d1e19aa550c8de3cef209fa060b62c16
konkam <guillaume.konkamking.work@gmail.com>
2020-03-25 16:05:30 +0100

    introduced robust Pkn

--- src/Pkn.jl
+++ src/Pkn.jl
@@ -1,4 +1,95 @@
 function Pkn_NGG_arb(k, n,  β, σ)
-    σ_arb = RR(σ)
-    Vnk_exact(n, k, β, σ) // σ_arb^k * Cnk(n, k, σ)
+    if k>n
+        return RR(0)
+    else
+        σ_arb = RR(σ)
+        Vnk_exact(n, k, β, σ) // σ_arb^k * Cnk(n, k, σ)
+    end
+end
+
+function has_reasonable_precision(arb_num)
+    return accuracy_bits(arb_num) ≥ 64
+end
+
+function Pkn_robust(k, n,  β, σ; verbose = false)
+    res = Pkn_NGG_arb(k, n,  β, σ)
+    if has_reasonable_precision(res)
+        return res
+    else
+        start_Pkn_val = res
+        i = 1
+        start_k_ind = undef
+        while !has_reasonable_precision(start_Pkn_val)
+            if 2^i*k>n
+                error("There seem to be a numerical problem with computing Pkn even for large values of k")
+            end
+            start_k_ind = 2^i*k
+            start_Pkn_val = Pkn_NGG_arb(start_k_ind, n,  β, σ)
+            i += 1
+        end
+        if verbose
+            println("k=$k, start_k_ind=$start_k_ind")
+        end
+        return Pkn_approx(k, n,  β, σ, start_k_ind, start_Pkn_val)
+    end
+end
+
+function log_βnk(β, n, k, σ)
+    return log(β) + log(n) - 1/σ*log(k)
+end
+βnk(β, n, k, σ) = exp(log_βnk(β, n, k, σ))
+function logxk(n, k, β, σ)
+    return log(k*σ + βnk(β, n, k, σ)) + log(Cnk(n, k+1, σ)) - log(σ) - log(Cnk(n, k, σ))
+end
+
+function Pkn_approx(k, n, β, σ, start_k_ind, start_Pkn_val)
+    if k==start_k_ind
+        return start_Pkn_val
+    else
+        logPkn = log(start_Pkn_val)
+        for ki in (start_k_ind-1):-1:k
+            # println(logPkn)
+            logPkn = logPkn - logxk(n, ki, β, σ)
+        end
+    #     return P1kn
+    #     [logprod_xi(n, j, β, σ) for j in 1:(n-1)]
+        return exp(logPkn)
+    end
+end
+
+Pkn_approx(k, n,  β, σ) = Pkn_approx(k, n,  β, σ, n, Pkn_NGG_arb(n, n,  β, σ))
+
+function Pkn_approx(n, β, σ, start_k_ind, start_Pkn_val)
+    logP1kn = Array{arb}(undef, start_k_ind)
+    logP1kn[start_k_ind] = start_Pkn_val |> log
+    for k in (start_k_ind-1):-1:1
+        logP1kn[k] = logP1kn[k+1] - logxk(n, k, β, σ)
+    end
+#     return P1kn
+#     [logprod_xi(n, j, β, σ) for j in 1:(n-1)]
+    return exp.(logP1kn)
+end
+
+Pkn_approx(n, β, σ) = Pkn_approx(n, β, σ, n, Pkn_NGG_arb(n, n,  β, σ))
+
+function Pkn_robust(n,  β, σ; verbose = false)
+    Pkn_val = Pkn_NGG_arb(n, n,  β, σ)
+    if !has_reasonable_precision(Pkn_val)
+        error("There seem to be a numerical problem with computing Pkn even for k=n")
+    else
+        P1n = Array{arb}(undef, n)
+        k = n
+        while k ≥ 1 && has_reasonable_precision(Pkn_val)
+            P1n[k] = Pkn_val
+            k -= 1
+            Pkn_val = Pkn_NGG_arb(k, n,  β, σ)
+        end
+        if verbose
+            println("k=$k, n=$n")
+        end
+        if k > 1
+            P1n[1:(k+1)] = Pkn_approx(n, β, σ, k+1, P1n[k+1])
+        end
+        return P1n
+    end
 end
--- test/test_Pkn.jl
+++ test/test_Pkn.jl
@@ -1,1 +1,10 @@
 @test_nowarn GibbsTypePriors.Pkn_NGG_arb(50, 100, 0.5, 0.2)
+@test GibbsTypePriors.log_βnk(1.3, 100, 25, 1.2) ≈ 2.1851379297320825
+@test Float64(GibbsTypePriors.logxk(10, 5, 1.3, 0.9))  ≈ 0.7300787963127003
+@test_nowarn GibbsTypePriors.Pkn_approx(90, 100,  1.2, GibbsTypePriors.RR(0.6))
+@test_nowarn GibbsTypePriors.Pkn_approx(100, 100,  1.2, GibbsTypePriors.RR(0.6))
+@test_nowarn GibbsTypePriors.Pkn_robust(100, 100,  1.2, GibbsTypePriors.RR(0.6))
+@test_nowarn GibbsTypePriors.Pkn_robust(1, 100,  1.2, GibbsTypePriors.RR(0.6))
+@test_nowarn GibbsTypePriors.Pkn_robust(1, 1000,  1.2, GibbsTypePriors.RR(0.6))
+@test_nowarn GibbsTypePriors.Pkn_approx(20, 1.2, 0.6)
+@test_nowarn GibbsTypePriors.Pkn_robust(20, 1.2, 0.6; verbose = true)
