04e19634c894c768c87c140892a7ee46f1d4420c
konkam <guillaume.konkamking.work@gmail.com>
2020-03-30 17:12:07 +0200

    added stirling number fun

--- src/common_functions.jl
+++ src/common_functions.jl
@@ -1,4 +1,4 @@
-using SpecialFunctions, Nemo, StatsFuns
+using SpecialFunctions, Nemo, StatsFuns, Memoization
 import Nemo.binom, Nemo.gamma
 
 prec = 5000 ## Increase for better precision
@@ -6,3 +6,21 @@ RR = RealField(prec)
 CC = ComplexField(prec)
 binom(n::Int64, k::Int64, r::Nemo.ArbField) = binom(convert(UInt64, n),convert(UInt64, k), r)
 gamma(x::Int64) = Nemo.gamma(RR(x))
+
+@memoize function unsigned_Stirling1(n::Integer,k::Integer)
+  # special cases
+  if k<0 || n<0
+    throw(DomainError())
+  end
+  if k>n
+    return big(0)
+  end
+  if n==0  # and, by logic, k==0
+    return big(1)
+  end
+  if k==0  # and, by logic, n>0
+    return big(0)
+  end
+  # end of special cases, invoke recursion
+  return unsigned_Stirling1(n-1,k-1) + (n-1)*unsigned_Stirling1(n-1,k)
+end
