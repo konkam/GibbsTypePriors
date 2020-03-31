c2d596f072bb313fe76efc35cc796ba78c30780b
konkam <guillaume.konkamking.work@gmail.com>
2020-03-31 11:51:50 +0200

    plot error on Pkn due to approx

--- Makefile
+++ Makefile
@@ -31,8 +31,20 @@ test/graphical_tests/saves_for_graphical_tests/accuracy_Cnk_10000.jld: test/grap
 test/graphical_tests/figures_graphical_tests/accuracy_PknCnkVnk_10000.pdf: test/graphical_tests/saves_for_graphical_tests/accuracy_Vnk_10000.jld test/graphical_tests/saves_for_graphical_tests/accuracy_Pkn_10000.jld test/graphical_tests/saves_for_graphical_tests/accuracy_Cnk_10000.jld test/graphical_tests/accuracy_PknCnkVnk_10000_plt.jl
 	julia test/graphical_tests/accuracy_PknCnkVnk_10000_plt.jl
 
+### Plot Pkn approx quality decrease
+
+test/graphical_tests/figures_graphical_tests/Pkn_NGG_approx_quality.pdf: test/graphical_tests/Pkn_NGG_approx_quality.jl
+	julia test/graphical_tests/Pkn_NGG_approx_quality.jl
+
+
 README.md: README.jmd
 	julia -e 'using Weave; weave("README.jmd", out_path=:pwd)'
+
+### Shortcut
+
+graphical_tests: test/graphical_tests/figures_graphical_tests/accuracy_PknCnkVnk_1000.pdf test/graphical_tests/figures_graphical_tests/accuracy_PknCnkVnk_10000.pdf test/graphical_tests/figures_graphical_tests/accuracy_Cnk_sigma.pdf test/graphical_tests/figures_graphical_tests/dist_Pkn_1000.pdf test/graphical_tests/figures_graphical_tests/Pkn_NGG_approx_quality.pdf
+.PHONY: graphical_tests
+
 README: README.md
 .PHONY: README
 
---
+++ test/graphical_tests/Pkn_approx_quality.jl
@@ -0,0 +1,40 @@
+using Nemo, GibbsTypePriors, RCall, JLD
+
+grid_k = 1:100
+
+n, β, σ = 100, 1.2, 0.6
+
+
+exact_Pkn = GibbsTypePriors.Pkn_NGG_arb.(grid_k, n,  β, σ)
+approx_Pkn = GibbsTypePriors.Pkn_NGG_approx.(grid_k, n, β, σ, n, exact_Pkn[n])
+
+R"library(tidyverse)"
+R"p2 = tibble(k = $grid_k,
+         exact_Pkn = $(Float64.(exact_Pkn)),
+         approx_Pkn = $(Float64.(approx_Pkn))) %>%
+         #mutate(abs_rel_error = abs(approx_Pkn-exact_Pkn)/exact_Pkn) %>%
+         mutate(Error = approx_Pkn-exact_Pkn) %>%
+         #ggplot(aes(x = k, y = abs_rel_error)) +
+         ggplot(aes(x = k, y = Error)) +
+         theme_bw() +
+         geom_point() +
+         geom_line()"
+
+R"p1 = tibble(k = $grid_k,
+        exact_Pkn = $(Float64.(exact_Pkn)),
+        approx_Pkn = $(Float64.(approx_Pkn))) %>%
+        gather(type, value, -k) %>%
+        mutate(type = gsub('_Pkn', '', type)) %>%
+        ggplot(aes(x = k, y = value, colour = type, group = type)) +
+        theme_bw() +
+        geom_point() +
+        geom_line() +
+        theme(legend.position = 'left') +
+        scale_colour_discrete(name='') +
+        ylab('Pkn')"
+
+R"p = gridExtra::grid.arrange(p1, p2, nrow = 1)"
+
+R"pdf('test/graphical_tests/figures_graphical_tests/Pkn_NGG_approx_quality.pdf', height = 5, width = 7)
+        plot(p)
+dev.off()"
