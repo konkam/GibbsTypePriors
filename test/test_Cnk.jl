3a5be100392ac2e111b906648a97f8cb3ee3d7ff
konkam <guillaume.konkamking.work@gmail.com>
2020-04-07 15:10:57 +0200

    more tests to improve coverage

--- test/test_Cnk.jl
+++ test/test_Cnk.jl
@@ -1,5 +1,8 @@
 @test_nowarn GibbsTypePriors.Cnk(6, 5, 0.5)
 @test_nowarn GibbsTypePriors.Cnk(6, 5, GibbsTypePriors.RR(0.5))
+@test_nowarn GibbsTypePriors.Cnk(6, 5, GibbsTypePriors.RR(0.5))
 
 @test Float64(GibbsTypePriors.Cnk(6, 5, 0.5)) == Float64(GibbsTypePriors.Cnk_rec(6, 5, 0.5))
 @test Float64(GibbsTypePriors.Cnk(600, 599, 0.5)) == Float64(GibbsTypePriors.Cnk_rec(600, 599, 0.5))
+@test Float64(GibbsTypePriors.Cnk(6, 5, 0.5)) == Float64(GibbsTypePriors.Cnk_rec(6, 5, GibbsTypePriors.RR(0.5)))
+@test Float64(GibbsTypePriors.Cnk(600, 599, 0.5)) == Float64(GibbsTypePriors.Cnk_rec(600, 599, GibbsTypePriors.RR(0.5)))
--- test/test_Expect_Kn.jl
+++ test/test_Expect_Kn.jl
@@ -1,2 +1,3 @@
 @test_nowarn GibbsTypePriors.expected_number_of_clusters_Dirichlet(500, 60., 500)
 @test_nowarn GibbsTypePriors.expected_number_of_cluster_2PD(10, 0., 0.4, 20)
+@test_nowarn GibbsTypePriors.expected_number_of_cluster_stable(10, 0.4, 20)
