6f7293b05c2b046120c4f266337d24311541f4ee
konkam <guillaume.konkamking.work@gmail.com>
2020-04-08 18:10:39 +0200

    changed commit benchmarked

--- run_benchmarks.jl
+++ run_benchmarks.jl
@@ -5,5 +5,7 @@ results = benchmarkpkg("/home/gkonkamking/Work/GibbsTypePriors", retune = true)
 PkgBenchmark.export_markdown("benchmark_results.md", results)
 
 judge_res = judge("GibbsTypePriors",
-      "2611aaf8",
+      "0510ea6edd45ad53c4bf8c7490239bdfb3fe0f72",
       "0115344fe322cdfbe4c83af5da982399d281979b")
+
+PkgBenchmark.export_markdown("benchmark_cmp.md", judge_res)
