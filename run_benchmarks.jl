18cc19ee9c2594875072548d555a9d973cd5fa4d
konkam <guillaume.konkamking.work@gmail.com>
2020-04-08 17:48:27 +0200

    comparison

--- run_benchmarks.jl
+++ run_benchmarks.jl
@@ -3,3 +3,7 @@ import GibbsTypePriors
 
 results = benchmarkpkg("/home/gkonkamking/Work/GibbsTypePriors", retune = true)
 PkgBenchmark.export_markdown("benchmark_results.md", results)
+
+judge_res = judge("GibbsTypePriors",
+      "2611aaf8",
+      "0115344fe322cdfbe4c83af5da982399d281979b")
