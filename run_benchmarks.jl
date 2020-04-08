fb61e67506a4366963c7f32044f79b089b1f6dfc
konkam <guillaume.konkamking.work@gmail.com>
2020-04-08 18:30:52 +0200

    PkgBenchmark refuses to benchmark without committing

--- run_benchmarks.jl
+++ run_benchmarks.jl
@@ -3,3 +3,9 @@ import GibbsTypePriors
 
 results = benchmarkpkg("/home/gkonkamking/Work/GibbsTypePriors", retune = true)
 PkgBenchmark.export_markdown("benchmark_results.md", results)
+
+judge_res = judge("GibbsTypePriors",
+      "7ec7963270ffe82d8958166ebb70c0facadb1233",
+      "0115344fe322cdfbe4c83af5da982399d281979b")
+
+PkgBenchmark.export_markdown("benchmark_cmp.md", judge_res)
