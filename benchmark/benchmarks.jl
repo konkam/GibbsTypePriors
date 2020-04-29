4749ffce09c873108fa1958b8632c8c60e73eb2b
konkam <guillaume.konkamking.work@gmail.com>
2020-04-29 18:30:06 +0200

    removed some baseline benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -16,21 +16,21 @@ SUITE["utf8"]["replace"] = @benchmarkable replace($teststr, "a" => "b")
 SUITE["utf8"]["join"] = @benchmarkable join($teststr, $teststr)
 SUITE["utf8"]["plots"] = BenchmarkGroup()
 
-# Add some benchmarks to the "trig" group
-SUITE["trigonometry"] = BenchmarkGroup(["math", "triangles"])
-SUITE["trigonometry"]["circular"] = BenchmarkGroup()
-for f in (sin, cos, tan)
-    for x in (0.0, pi)
-        SUITE["trigonometry"]["circular"][string(f), x] = @benchmarkable ($f)($x)
-    end
-end
-
-SUITE["trigonometry"]["hyperbolic"] = BenchmarkGroup()
-for f in (sin, cos, tan)
-    for x in (0.0, pi)
-        SUITE["trigonometry"]["hyperbolic"][string(f), x] = @benchmarkable ($f)($x)
-    end
-end
+# # Add some benchmarks to the "trig" group
+# SUITE["trigonometry"] = BenchmarkGroup(["math", "triangles"])
+# SUITE["trigonometry"]["circular"] = BenchmarkGroup()
+# for f in (sin, cos, tan)
+#     for x in (0.0, pi)
+#         SUITE["trigonometry"]["circular"][string(f), x] = @benchmarkable ($f)($x)
+#     end
+# end
+#
+# SUITE["trigonometry"]["hyperbolic"] = BenchmarkGroup()
+# for f in (sin, cos, tan)
+#     for x in (0.0, pi)
+#         SUITE["trigonometry"]["hyperbolic"][string(f), x] = @benchmarkable ($f)($x)
+#     end
+# end
 
 SUITE["Cnk"] = BenchmarkGroup()
 SUITE["Cnk"]["direct"] = @benchmarkable GibbsTypePriors.Cnk(6, 5, 0.5)
