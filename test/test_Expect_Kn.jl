c0e0ff0c2bc9c9764364fbef08aa1ee8ac14f714
konkam <guillaume.konkamking.work@gmail.com>
2020-09-29 22:35:41 +0200

    try to improve coverage

--- test/test_Expect_Kn.jl
+++ test/test_Expect_Kn.jl
@@ -5,7 +5,7 @@
 @test_nowarn GibbsTypePriors.expected_number_of_cluster_stable(10, 0.4, 20)
 @test_nowarn GibbsTypePriors.expected_number_of_cluster_stable(10, 0.4)
 
-@test_nowarn GibbsTypePriors.E_Dirichlet(500, 60., 500)
-@test_nowarn GibbsTypePriors.E_2PD(10, 0., 0.4, 20)
-@test_nowarn GibbsTypePriors.E_PY(10, 0., 0.4, 20)
-@test_nowarn GibbsTypePriors.E_stable(10, 0.4, 20)
+@test_nowarn GibbsTypePriors.E_Dirichlet(500, 60.)
+@test_nowarn GibbsTypePriors.E_2PD(10, 0., 0.4)
+@test_nowarn GibbsTypePriors.E_PY(10, 0., 0.4)
+@test_nowarn GibbsTypePriors.E_stable(10, 0.4)
