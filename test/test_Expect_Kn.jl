9afdcacbc8c51be2e807180c1454c29cea94dbf5
konkam <guillaume.konkamking.work@gmail.com>
2020-09-29 18:04:53 +0200

    added doc and more tests for Expect_Kn.jl

--- test/test_Expect_Kn.jl
+++ test/test_Expect_Kn.jl
@@ -1,6 +1,9 @@
-@test_nowarn GibbsTypePriors.expected_number_of_clusters_Dirichlet(500, 60., 500)
+@test_nowarn GibbsTypePriors.expected_number_of_clusters_Dirichlet(500, 60.)
+@test_nowarn GibbsTypePriors.expected_number_of_clusters_Dirichlet(500, 60., 50)
 @test_nowarn GibbsTypePriors.expected_number_of_cluster_2PD(10, 0., 0.4, 20)
+@test_nowarn GibbsTypePriors.expected_number_of_cluster_2PD(10, 0., 0.4)
 @test_nowarn GibbsTypePriors.expected_number_of_cluster_stable(10, 0.4, 20)
+@test_nowarn GibbsTypePriors.expected_number_of_cluster_stable(10, 0.4)
 
 @test_nowarn GibbsTypePriors.E_Dirichlet(500, 60., 500)
 @test_nowarn GibbsTypePriors.E_2PD(10, 0., 0.4, 20)
