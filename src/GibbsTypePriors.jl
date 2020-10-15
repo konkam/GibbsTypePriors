f981da495540cd27d39fec14341a8e1c65cb00d9
konkam <guillaume.konkamking.work@gmail.com>
2020-10-15 16:40:30 +0200

    Added variable precision versions of the functions

--- src/GibbsTypePriors.jl
+++ src/GibbsTypePriors.jl
@@ -4,7 +4,10 @@ export Pkn_NGG, Pkn_2PD, Pkn_PY, Pkn_Dirichlet, E_2PD, E_PY, E_stable, E_Dirichl
 
 include("common_functions.jl")
 include("Cnk.jl")
+include("Cnk_variable_prec.jl")
 include("Vnk.jl")
+include("Vnk_variable_prec.jl")
 include("Pkn.jl")
+include("Pkn_variable_prec.jl")
 include("Expect_Kn.jl")
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,11 @@
 using GibbsTypePriors
-using Test
+using Test, Nemo
 
 include("test_common_functions.jl")
 include("test_Cnk.jl")
+include("test_Cnk_variable_prec.jl")
 include("test_Vnk.jl")
+include("test_Vnk_variable_prec.jl")
 include("test_Pkn.jl")
+include("test_Pkn_variable_prec.jl")
 include("test_Expect_Kn.jl")
