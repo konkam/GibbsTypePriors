f923ed8a365261c34f4749b75005764279e63c94
bystrova <bystrova.daria@gmail.com>
2021-04-14 13:06:15 +0200

    export DP, PY, NGG multinomial

--- src/GibbsTypePriors.jl
+++ src/GibbsTypePriors.jl
@@ -1,6 +1,6 @@
 module GibbsTypePriors
 
-export Pkn_NGG, Pkn_2PD, Pkn_PY, Pkn_Dirichlet, E_2PD, E_PY, E_stable, E_Dirichlet
+export Pkn_NGG, Pkn_2PD, Pkn_PY, Pkn_Dirichlet, Pkn_Dirichlet_mult, Pkn_PYM, Pkn_NGGM, E_2PD, E_PY, E_stable, E_Dirichlet
 
 include("common_functions.jl")
 include("Cnk.jl")
